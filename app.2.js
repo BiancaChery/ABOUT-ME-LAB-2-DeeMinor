0d05402be6bb5232047412f0cde5307fd04a400a
ShaDesa Minor <missdeeminor@gmail.com>
2023-04-22 16:09:19 -0400

    updates the questions

--- app.2.js
+++ app.2.js
@@ -18,3 +18,20 @@ while (true) {
  alert(`you guessed ${attempts}`);
 }
 
+let favSeason;
+favSeason = "fall"
+let attempts2=0
+
+while (true) {
+  let favSeason = prompt("what is my favorite season?").toLowerCase();
+  
+  if (favSeason === attempts2) {
+    console.log("correct")
+    break;
+  } 
+  else { 
+  console.log ("try again");
+  attempts2=attempts2 + 1
+  }
+ alert(`you guessed ${attempts2}`);
+}
